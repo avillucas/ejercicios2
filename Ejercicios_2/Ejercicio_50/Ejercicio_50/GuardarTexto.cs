8df813aa1f9871ab0b08cfad04e348a0837f8693
alumno <alumno>
2018-05-16 22:14:54 -0300

    inicio del 50

---
+++ Ejercicios_2/Ejercicio_50/Ejercicio_50/GuardarTexto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_50
+{
+    class GuardarTexto : IGuardar
+    {
+         bool IGuardar.Guardar(T obj){
+             return true;
+         }
+         
+        V IGuardar.Leer() {
+             return "Texto Leído";
+         }
+    }
+}
