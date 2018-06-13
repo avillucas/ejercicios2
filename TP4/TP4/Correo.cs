6c4cfe46bbc57dfadda227f605384add8eec8b20
alumno <alumno>
2018-06-13 20:20:06 -0300

    inicio del ejemplo TP$

---
+++ TP4/TP4/Correo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TP4
+{
+    class Correo
+    {
+        List<Thread> mockPaquetes;
+        List<Paquete> paquetes;
+        
+        public List<Paquete> Paquetes
+        {
+            get {
+                return this.paquetes;
+            
+            }
+            set {
+                this.paquetes = value;
+            }
+        }
+
+        public Correo() 
+        {
+            this.paquetes = new List<Paquete>();
+        }
+
+        public void FinEntrega()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string MostrarDatos(IMostrar<List<Paquete>> elemento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Correo operator + ( Correo c, Paquete p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TP4/TP4/Paquete.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP4
+{
+    class Paquete
+    {
+        private string direccionEntrega;
+        private EEstado estado;
+        private string trackingID;
+        enum EEstado  {
+            Ingresado,
+            EnViaje, 
+            Entregado
+        }
+
+        public string DireccionEntrega
+        {
+            get
+            {
+                return direccionEntrega;
+            }
+            set
+            {
+                direccionEntrega = value;
+            }
+        }
+        public EEstado Estado
+        {
+            get
+            {
+                return estado;
+            }
+            set
+            {
+                estado = value;
+            }
+        }
+
+        public string TrackingID{
+            get
+            {
+                return trackingID;
+            }
+            set
+            {
+                trackingID = value;
+            }
+        }
+
+        public void MockCicloDeVida()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
